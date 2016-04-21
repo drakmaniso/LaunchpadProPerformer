f61863480e0b6cd3188bf3925846b0b25583b953
Laurent Moussault <moussault.laurent@gmail.com>
2016-04-21 17:38:50 +0200

    Add wide pads option for drums mode

--- menus/drums_layout_menu.js
+++ menus/drums_layout_menu.js
@@ -12,22 +12,13 @@ Drums_Layout_Menu.prototype.on_midi = function(status, data1, data2) {
     if (status == 0x90 && data2 > 0) {
         var y = d.pad_y(data1)
         if (y < 4) {
+            this.screen.mode.wide_pads = true
+            this.screen.mode.colors = drums_color_schemes[1]
         } else {
+            this.screen.mode.wide_pads = false
+            this.screen.mode.colors = drums_color_schemes[0]
         }
         this.draw_grid()
-    } else if (status == 0xb0 && data2 > 0) {
-		// switch (data1) {
-		// 	case 0x59:
-		// 		this.screen.mode.key_colors = key_color_schemes[0]
-		// 		break
-		// 	case 0x4f:
-		// 		this.screen.mode.key_colors = key_color_schemes[1]
-		// 		break
-		// 	case 0x45:
-		// 		this.screen.mode.key_colors = key_color_schemes[2]
-		// 		break
-		// }
-        // this.draw_grid()
     }
     
     return  ! (status == 0xb0 && data1 == 0x32 && data2 == 0x00)
@@ -51,17 +42,19 @@ Drums_Layout_Menu.prototype.draw_grid = function () {
     // d.set_page_button(1, kc === key_color_schemes[1] ? 0x12 : 0x13)
     // d.set_page_button(2, kc === key_color_schemes[2] ? 0x12 : 0x13)
 
-    d.clear_pads (0x00)    
-    // for (y = 4; y < 8; y++) {
-    //     for (x = 0; x < 8; x++) {
-    //         d.set_pad(x, y, (x + y) % 2 == 0 ? 0x01 : 0x00)
-    //     }
-    // }
-    // for (y = 0; y < 4; y++) {
-    //     for (x = 0; x < 8; x++) {
-    //         d.set_pad(x, y, (Math.floor(x/2) + y) % 2 == 0 ? 0x03 : 0x00)
-    //     }
-    // }
+    d.clear_pads(0x00)
+    var c = this.screen.mode.wide_pads ? 0x03 : 0x01
+    for (y = 4; y < 8; y++) {
+        for (x = 0; x < 8; x++) {
+            d.set_pad(x, y, (x + y) % 2 == 0 ? c : 0x00)
+        }
+    }
+    c = this.screen.mode.wide_pads ? 0x01 : 0x03
+    for (y = 0; y < 4; y++) {
+        for (x = 0; x < 8; x++) {
+            d.set_pad(x, y, (Math.floor(x/2) + y) % 2 == 0 ? c : 0x00)
+        }
+    }
 }
 
 //------------------------------------------------------------------------------
--- modes/drums_mode.js
+++ modes/drums_mode.js
@@ -7,31 +7,11 @@ function Drums_Mode(screen) {
 
     this.layout_menu = new Drums_Layout_Menu(screen)
 
+    this.wide_pads = false
+    this.colors = drums_color_schemes[0]
     this.translation = new_translation_table()
     this.origin = 36
 }
-
-
-const DRUM_COLORS = [
-    0x06, 0x06,
-    0x09, 0x09,
-    0x03, 0x03,
-    0x07, 0x07,
-    0x02, 0x02,
-    0x05, 0x05,
-    0x08, 0x08,
-    0x0b, 0x0b,
-]
-// const DRUM_COLORS = [
-//     0x06, 0x07,
-//     0x09, 0x0b,
-//     0x03, 0x04,
-//     0x07, 0x08,
-//     0x0b, 0x02,
-//     0x05, 0x06,
-//     0x08, 0x09,
-//     0x02, 0x03,
-// ]
 
 //------------------------------------------------------------------------------
 
@@ -40,11 +20,21 @@ Drums_Mode.prototype.on_midi = function(status, data1, data2) {
     if (status == 0xb0 && data2 > 0) {
         switch (data1) {
             case 0x5b:
-                this.origin = 36
+                if (this.wide_pads) {
+                    this.origin += 32
+                    this.origin = modulo(this.origin, 128)
+                } else {
+                    this.origin = 36
+                }    
                 h = true
                 break
             case 0x5c:
-                this.origin = 36 + 64
+                if (this.wide_pads) {
+                    this.origin -= 32
+                    this.origin = modulo(this.origin, 128)
+                } else {
+                    this.origin = 36 + 64
+                }
                 h = true
                 break
             case 0x5d:
@@ -61,18 +51,31 @@ Drums_Mode.prototype.on_midi = function(status, data1, data2) {
         if (h) {
             this.update_and_draw()
         }
-    } else if(status == 0x90) {
+    } else if (status == 0x90) {
+        h = true
         var d = this.screen.launchpad.display
         var x = d.pad_x(data1)
         var y = d.pad_y(data1)
         if (data2 > 0) {
-            d.set_pad(x, y, 0x0a)
+            if (this.wide_pads) {
+                x = 2 * Math.floor(x / 2)
+                d.set_pad(x, y, 0x0a)
+                d.set_pad(x+1, y, 0x0a)
+            } else {
+                d.set_pad(x, y, 0x0a)
+            }    
         } else {
             var n = this.pad_note(x, y)
             if (n > -1) {
                 n = modulo(n - 4, 128)
                 var sq = 2 * Math.floor(n / 16) + Math.floor(n + n / 4) % 2
-                d.set_pad(x, y, DRUM_COLORS[sq])
+                if (this.wide_pads) {
+                    x = 2 * Math.floor(x / 2)
+                    d.set_pad(x, y, this.colors[sq])
+                    d.set_pad(x + 1, y, this.colors[sq])
+                } else {
+                    d.set_pad(x, y, this.colors[sq])
+                }    
             }
         }
     }
@@ -113,7 +116,7 @@ Drums_Mode.prototype.draw_grid = function() {
             if (n > -1) {
                 n = modulo(n - 4, 128)
                 var sq = 2 * Math.floor(n / 16) + Math.floor(n + n / 4) % 2
-                d.set_pad(x, y, DRUM_COLORS[sq])
+                d.set_pad(x, y, this.colors[sq])
             } else {
                 d.set_pad(x, y, 0x00)
             } 
@@ -123,6 +126,9 @@ Drums_Mode.prototype.draw_grid = function() {
 
 Drums_Mode.prototype.pad_note = function (x, y) {
     var n = this.origin + 4 * y
+    if (this.wide_pads) {
+        x = Math.floor(x / 2)
+    }
     if (x > 3) {
         x -= 4
         n += 32
