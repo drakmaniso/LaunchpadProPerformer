ff3729c81e9a01d8e93d181260f9f6a938e04e05
Laurent Moussault <moussault.laurent@gmail.com>
2016-04-23 12:20:08 +0200

    Silence debug prints

--- main/bitwig.js
+++ main/bitwig.js
@@ -1,21 +1,34 @@
 //------------------------------------------------------------------------------
 
 function Bitwig() {
-	this.trackBank = host.createMainTrackBank(8, 1, 8)
+    this.trackBank = host.createMainTrackBank(8, 1, 8)
+    this.trackBank.addChannelCountObserver(function (c) {
+        // println("COUNT: " + c.toString())
+    })
     for (i = 0; i < 8; i++) {
         t = this.trackBank.getChannel(i)
+        t.addPositionObserver(function (bw, idx) {
+            return function (position) {
+                // println("Track " + idx.toString() + " is at " + position.toString())
+            }
+        } (this, i))
+        t.addNameObserver(16, "", function (bw, idx) {
+            return function (name) {
+                // println("Track " + idx.toString() + " is named " + name)
+            }
+        } (this, i))
         cls = t.getClipLauncherSlots() 
         cls.setIndication(true)
         cls.addHasContentObserver(function (bw, track) {
             return function (slot, has_clip) {
-                println("HasContent: " + track.toString() + ", " + slot.toString() + ": " + has_clip.toString())
+                // println("HasContent: " + track.toString() + ", " + slot.toString() + ": " + has_clip.toString())
                 bw.on_has_content(track, slot, has_clip)
             }
         } (this, i) )
     }
     // act = host.createArrangerCursorTrack(0, 8)
 }
-
+ 
 //------------------------------------------------------------------------------
 
 Bitwig.prototype.setLaunchpad = function (launchpad) {
@@ -25,7 +38,7 @@ Bitwig.prototype.setLaunchpad = function (launchpad) {
 //------------------------------------------------------------------------------
 
 Bitwig.prototype.on_has_content = function (track, slot, has_clip) {
-    this.launchpad.display.set_pad(track, 7 - slot, has_clip ? 0x02 : 0x00)
+    // this.launchpad.display.set_pad(track, 7 - slot, has_clip ? 0x02 : 0x00)
 }
 
 //------------------------------------------------------------------------------
